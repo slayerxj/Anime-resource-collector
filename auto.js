e4536c16b57c4ee16e37adcbec1c3e12d0096b41
Allen Xiong <allen.xiong@sap.com>
2021-01-21 16:43:01 +0800

    add auto js

---
+++ auto.js
@@ -0,0 +1,13 @@
+
+var Database = require("./database.js");
+var sites = require("./websites/index.js");
+
+var database = new Database();
+database.initialize().rank();
+
+Object.keys(sites).forEach(function (domain) {
+    sites[domain].fetchNew(database, function () {
+        database.rankAll();
+        database.updateRecord();
+    });
+});
