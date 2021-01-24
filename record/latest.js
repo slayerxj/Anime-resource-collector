2cb1f49935ed69e68ba4c3a25f222ec6d83dddc2
slayerxj_reg@outlook.com <runner@fv-az151-730.u3d4e5dqw3qejehmufpjdgw0gd.bx.internal.cloudapp.net>
2021-01-24 20:24:28 +0000

    auto: routine update

--- record/latest.js
+++ record/latest.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-"name": "\n        [SAIO-Raws] 龙与魔女 BURN THE WITCH [BD 1920x1080 x264 FLACx2]",
+"name": "\n        [路基艾尔][家入レオ 7th Live Tour 2019 ～DUO～][正片+特典+SP][1080P][BDRip][HEVC-10bit][FLAC][MKV]",
 
 "workName": "",
 
@@ -10,15 +10,15 @@ module.exports = {
 
 "subtitleProvider": "",
 
-"url": "http://www.kisssub.org/show-653dfa352e8cb2e915f94967c410e39449872ba1.html",
+"url": "http://www.kisssub.org/show-8817337068e7f57bb1a7845c827eee169e8ae72a.html",
 
 "isNew": true,
 
-"publishTime": "2021-01-24T03:28:11.000Z",
+"publishTime": "2021-01-24T13:03:38.000Z",
 
 "isComplete": false,
 
 "generalRanking": 3,
 
-"magnetLink": "magnet:?xt=urn:btih:653dfa352e8cb2e915f94967c410e39449872ba1&tr=http://open.acgtracker.com:1096/announce"
+"magnetLink": "magnet:?xt=urn:btih:8817337068e7f57bb1a7845c827eee169e8ae72a&tr=http://open.acgtracker.com:1096/announce"
 }
