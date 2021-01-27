ecb020ee53478d0550aa5da3d76154b12e5087f3
Xiong Jie <slayerxj_reg@outlook.com>
2021-01-27 16:49:58 +0800

    feat: archive some records and refactor the file structure

--- auto.js
+++ auto.js
@@ -1,12 +1,12 @@
-var Database = require("./database.js");
+var Database = require("./src/database.js");
 var sites = require("./websites/index.js");
 
 var database = new Database();
 database.initialize().rank();
 
 Object.keys(sites).forEach(function (domain) {
-    sites[domain].fetchNew(database, function () {
-        database.rankAll();
-        database.updateRecord();
-    });
+  sites[domain].fetchNew(database, function () {
+    database.rankAll();
+    database.updateRecord();
+  });
 });
---
+++ src/urlFetcher.js
@@ -0,0 +1,97 @@
+var request = require("superagent");
+
+var urlQueue = [];
+var concurrencyCount = { value: 0 };
+var urlFailCount = {};
+var failedUrl = [];
+var doWhenFinish = null;
+
+var retry = 15;
+var maxConcurrencyNum = 2;
+
+var increaseFailCount = function (url) {
+    if (urlFailCount[url]) {
+        urlFailCount[url]++;
+    } else {
+        urlFailCount[url] = 1;
+    }
+};
+
+var handleFetchUrlFailed = function (url, callback) {
+    increaseFailCount(url);
+
+    if (urlFailCount[url] < retry) {
+        if (urlQueue.length === 0) {
+            fetchUrl(url, callback);
+        } else {
+            pushUrlToQueue(url, callback("Get the origin callback"));
+        }
+    } else {
+        failedUrl.push(url);
+        callback("err");
+    }
+};
+
+var fetchUrl = function (url, callback) {
+    concurrencyCount.value++;
+    console.log("Start fetching", url);
+    request
+        .get(url)
+        .timeout(10000)
+        .end(function (err, res) {
+            concurrencyCount.value--;
+            if (err) {
+                handleFetchUrlFailed(url, callback);
+            } else {
+                console.log("          ", url, "is loaded");
+                callback(res.text);
+            }
+        });
+};
+
+var pushUrlToQueue = function (url, callback) {
+    urlQueue.push({ url, callback });
+    if (concurrencyCount.value === 0) {
+        continueFetchingUrls();
+    }
+};
+
+var startFetchingUrls = function (finish) {
+    doWhenFinish = finish;
+    continueFetchingUrls();
+};
+
+var continueFetchingUrls = function () {
+    while ((urlQueue.length > 0) && (concurrencyCount.value < maxConcurrencyNum)) {
+        var fetchPack = urlQueue.shift();
+
+        var makeCallback = function (fetchPack) {
+            return function (res) {
+                if (res === "Get the origin callback") {
+                    return fetchPack.callback;
+                }
+                fetchPack.callback(res);
+
+                if ((concurrencyCount.value === 0) && (urlQueue.length === 0)) {
+                    if (failedUrl.length > 0) {
+                        console.log("The following url(s) is failed");
+                        failedUrl.forEach(function (url) {
+                            console.log(url);
+                        });
+                    } else if (doWhenFinish) {
+                        doWhenFinish();
+                        doWhenFinish = null;
+                    }
+                } else {
+                    continueFetchingUrls();
+                }
+            };
+        };
+        fetchUrl(fetchPack.url, makeCallback(fetchPack));
+    }
+};
+
+module.exports = {
+    pushUrlToQueue,
+    startFetchingUrls
+}
