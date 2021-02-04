909542963ff88a90431b86f11e4f758c968483ec
Allen Xiong <allen.xiong@sap.com>
2021-02-04 15:33:13 +0800

    refactor: better table content generating

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-const stars = [
-  "&#9733;&#9733;&#9733;&#9733;&#9733;",
-  "&#9733;&#9733;&#9733;&#9733;",
-  "&#9733;&#9733;&#9733;",
-  "&#9733;&#9733;",
-  "&#9733;",
-];
-
-const convertToTimeString = (date) => date.getFullYear() + "." + (date.getMonth() + 1) + "." + date.getDate();
+const star = "&#9733;";
 
 const sorter = (a, b) => new Date(b.publishTime).getTime() - new Date(a.publishTime).getTime();
+
+const convertToTimeString = (date) => date.getFullYear() + "." + (date.getMonth() + 1) + "." + date.getDate();
 
 const generateTableContent = (items) => {
   const table = document.getElementById("mainView");
   table.innerHTML =
     "<tr><th>Time</th><th>Title</th><th>Rank</th><th>Download</th></tr>" +
-    items
-      .map((cur) => {
-        return `<tr><td>${convertToTimeString(
-          new Date(cur.publishTime)
-        )}</td><td><a href='${cur.url}'>${cur.name}</a></td><td>${stars[cur.generalRanking]
-          }</td><td><a href='${cur.magnetLink}'>link</a></td></tr>`;
-      })
-      .join("");
+    items.map((item) =>
+      `<tr>
+        <td>${convertToTimeString(new Date(item.publishTime))}</td>
+        <td><a href='${item.url}'>${item.name}</a></td>
+        <td>${star.repeat(5 - item.generalRanking)}</td>
+        <td><a href='${item.magnetLink}'>link</a></td>
+      </tr>`
+    ).join("");
 }
 
 fetch('result.json')
