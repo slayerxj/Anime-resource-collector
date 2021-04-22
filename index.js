d02d0967cb74428419f5e1185e8b26a125d432ae
Allen Xiong <allen.xiong@sap.com>
2021-04-22 17:01:35 +0800

    feat: use download link instead of page link when clicking the title

--- index.js
+++ index.js
@@ -6,25 +6,27 @@ const stars = [
   "&#9733;",
 ];
 
-const convertToTimeString = (date) => date.getFullYear() + "." + (date.getMonth() + 1) + "." + date.getDate();
+const convertToTimeString = (date) =>
+  date.getFullYear() + "." + (date.getMonth() + 1) + "." + date.getDate();
 
-const sorter = (a, b) => new Date(b.publishTime).getTime() - new Date(a.publishTime).getTime();
+const sorter = (a, b) =>
+  new Date(b.publishTime).getTime() - new Date(a.publishTime).getTime();
 
 const generateTableContent = (items) => {
   const table = document.getElementById("mainView");
   table.innerHTML =
-    "<tr><th>Time</th><th>Title</th><th>Rank</th><th>Download</th></tr>" +
+    "<tr><th>Time</th><th>Title</th><th>Rank</th></tr>" +
     items
       .map((cur) => {
         return `<tr><td>${convertToTimeString(
           new Date(cur.publishTime)
-        )}</td><td><a href='${cur.url}'>${cur.name}</a></td><td>${stars[cur.generalRanking]
-          }</td><td><a href='${cur.magnetLink}'>link</a></td></tr>`;
+        )}</td><td><a href='${cur.magnetLink}'>${cur.name}</a></td><td>${stars[cur.generalRanking]
+          }</td></tr>`;
       })
       .join("");
-}
+};
 
-fetch('result.json')
-  .then(response => response.json())
-  .then(data => data.sort(sorter))
+fetch("result.json")
+  .then((response) => response.json())
+  .then((data) => data.sort(sorter))
   .then(generateTableContent);
